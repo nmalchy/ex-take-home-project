63f503d86611979cf3b93aab7b3be0072434f55a
Noah Malchy <50896885+nmalchy@users.noreply.github.com>
2024-08-11 12:44:32 -0700

    add loading to button (post-submission pls forgive it was driving me nuts)

--- frontend/src/components/SanctionsScreeningForm.tsx
+++ frontend/src/components/SanctionsScreeningForm.tsx
@@ -10,6 +10,7 @@ const SanctionsScreeningForm: React.FC = () => {
     const [matchResult, setMatchResult] = useState<MatchResult | null>(null);
     const [responseMessage, setResponseMessage] = useState<string>('');
     const [errorMessage, setErrorMessage] = useState<string>('');
+    const [loading, setLoading] = useState<boolean>(false);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
         const { name, value } = e.target;
@@ -29,6 +30,8 @@ const SanctionsScreeningForm: React.FC = () => {
             return;
         }
 
+        setLoading(true); // Start loading
+
         await apiClient.post<FormData, { data: SanctionsScreeningAPIResponse }>('/api/v1/check-sanctions-list', formData)
             .then((response: { data: SanctionsScreeningAPIResponse }) => {
                 if (response.data.isHit) {
@@ -46,7 +49,7 @@ const SanctionsScreeningForm: React.FC = () => {
                 setErrorMessage(`${axiosError}`);
                 setMatchResult(null); // Reset state on error
                 setResponseMessage(''); // Reset state on error
-            });
+            }).finally(() => setLoading(false));
     };
 
     return (
@@ -65,7 +68,9 @@ const SanctionsScreeningForm: React.FC = () => {
                     <label htmlFor="country">Country:</label>
                     <input id="country" type="text" name="country" value={formData.country} onChange={handleChange} />
                 </div>
-                <button type="submit">Submit</button>
+                <button type="submit" disabled={loading}>
+                    {loading ? <span className="spinner"></span> : 'Submit'}
+                </button>
             </form>
 
             {errorMessage && (
